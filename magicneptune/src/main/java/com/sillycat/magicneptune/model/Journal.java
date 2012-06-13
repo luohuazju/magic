66f3c7e792325153f80cf6590efef4b6148d9f63
luohuazju <luohuazju@gmail.com>
2012-06-13 18:35:08 +0800

    log model

--- magicneptune/src/main/java/com/sillycat/magicneptune/model/Journal.java
+++ magicneptune/src/main/java/com/sillycat/magicneptune/model/Journal.java
@@ -1,9 +1,83 @@
 package com.sillycat.magicneptune.model;
 
-public class Journal extends BasicObject{
+public class Journal extends BasicObject {
 
 	private static final long serialVersionUID = -7761413299035790582L;
 
-	
-	
+	private String username;
+
+	private String deviceid;
+
+	/**
+	 * view, like, buy
+	 */
+	private String actiontype;
+
+	private String merchantname;
+
+	private String productcategory;
+
+	private String productname;
+
+	/**
+	 * raw data from the clients
+	 */
+	private String rawdata;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getDeviceid() {
+		return deviceid;
+	}
+
+	public void setDeviceid(String deviceid) {
+		this.deviceid = deviceid;
+	}
+
+	public String getActiontype() {
+		return actiontype;
+	}
+
+	public void setActiontype(String actiontype) {
+		this.actiontype = actiontype;
+	}
+
+	public String getMerchantname() {
+		return merchantname;
+	}
+
+	public void setMerchantname(String merchantname) {
+		this.merchantname = merchantname;
+	}
+
+	public String getProductcategory() {
+		return productcategory;
+	}
+
+	public void setProductcategory(String productcategory) {
+		this.productcategory = productcategory;
+	}
+
+	public String getProductname() {
+		return productname;
+	}
+
+	public void setProductname(String productname) {
+		this.productname = productname;
+	}
+
+	public String getRawdata() {
+		return rawdata;
+	}
+
+	public void setRawdata(String rawdata) {
+		this.rawdata = rawdata;
+	}
+
 }
