e8451b01460f7d6925ad53d3456610ca4e5c3b19
carl <4myhappylife@gmail.com>
2013-06-03 23:10:15 -0500

    another auth is working

--- winner-seller-web-client/app/js/models/NavBarsModel.js
+++ winner-seller-web-client/app/js/models/NavBarsModel.js
@@ -16,10 +16,9 @@ define([
            //options.dataType = 'json';
 
            //jsonp
-           //options.headers = "{ 'Authorization' : 'Basic Y3VzdG9tZXI6Y3VzdG9tZXI=' }";
            options.dataType = "jsonp";
            options.crossDomain = true;
-           options.url = 'http://localhost:9002/v1/sillycat' + "/navbars";
+           options.url = 'http://' + 'customer' + ':' + 'customer' + '@' + 'localhost:9002/v1/sillycat' + '/navbars';
 
            return Backbone.sync(method, model, options);
         }
