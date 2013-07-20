17c5eef835e284f6783c98184085a3c5e06582f4
Carl <luohuazju@gmail.com>
2013-07-20 14:45:52 -0500

    login is working

--- winner-seller-server/src/main/scala/com/sillycat/winnersellerserver/bootstrap/UserRouterService.scala
+++ winner-seller-server/src/main/scala/com/sillycat/winnersellerserver/bootstrap/UserRouterService.scala
@@ -34,6 +34,9 @@ trait UserRouterService extends BaseRouterService with CustomerMethodDirectives 
 
         respondWithHeaders(SillycatUtil.getCrossDomainHeaders(originHeader): _*) {
 
+          //hostName { webserver =>
+          host("([a-zA-Z0-9]*).api.sillycat.com".r) { webserver =>
+            println("I get it webserver = " + webserver)
             path("auth") {
               post {
                 entity(as[UserLogon]) { item =>
@@ -52,6 +55,7 @@ trait UserRouterService extends BaseRouterService with CustomerMethodDirectives 
                 }
               }
             }
+          }
         }
       }
     }
